2260c70512b8b6f0696a685d4f72f7d3e9e0b5dc
Thomas Wood <odell.wood@gmail.com>
2019-01-11 16:06:55 -0800

    added sample_subgraph and get_graph to reconstruct saved redisgraph into a nx.Graph

--- data_utils.py
+++ data_utils.py
@@ -8,36 +8,19 @@
 from io import BytesIO
 from pprint import pprint
 import time
+from uuid import uuid4
 
 # Redis modules
 import redis
 from redisgraph import Node, Edge, Graph
+from redisgraph.query_result import QueryResult
 
 # Numerical modules
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
-
-
-"""
-########################
-### EXAMPLE USAGE!!! ###
-########################
-r = redis.Redis(host='localhost', port=6379)
-redis_graph = Graph('social', r)
-
-john = Node(label='person', properties={'name': 'John Doe', 'age': 33, 'gender': 'male', 'status': 'single'})
-redis_graph.add_node(john)
-
-japan = Node(label='country', properties={'name': 'Japan'})
-redis_graph.add_node(japan)
-
-edge = Edge(john, 'visited', japan, properties={'purpose': 'pleasure'})
-redis_graph.add_edge(edge)
-
-redis_graph.commit()
-"""
-
-"""
+my_thoughts = """
 So the idea is:
   1. Get an entire adjacency matrix back from redis_graph.
   2. Store numeric data for the nodes in redis using a common key/index.
@@ -49,6 +32,16 @@ features and figure out how to do this thing first. Then we can upgrade to using
 the same general formula to create the VOC gold standard region graph dataset.
 """
 
+def _gen_key(key_len):
+    return uuid4().hex[:key_len]
+
+def _index_key(index_dict, key_len=10):
+    index_key = _gen_key(key_len)
+    while index_key in index_dict:
+        index_key = _gen_key(key_len)
+    index_dict[index_key] = 1
+    return index_key
+
 def _array2bytes(arr):
     with BytesIO() as b:
         np.save(b, arr)
@@ -59,14 +52,15 @@ def _bytes2array(byts):
 
 def _add_nodes(r, graph, n_nodes):
     nodes = []
+    index_dict = {}
     for k in range(n_nodes):
-        n = Node(label="node")
+        index_key = _index_key(index_dict)
+        n = Node(label="node", properties={"index_key":index_key})
         graph.add_node(n)
         nodes.append(n)
-        _id = n.alias
         feat = np.random.rand(1,100,7,7)
         feat_bytes = _array2bytes(feat)
-        r.set(_id, feat_bytes)
+        r.set(index_key, feat_bytes)
     # graph.commit()
     return nodes
 
@@ -74,7 +68,7 @@ def _add_edges(nodes, graph, edge_prob):
     t = time.time()
     edges = []
     for k, node0 in enumerate(nodes):
-        for kk, node1 in enumerate(nodes):
+        for kk, node1 in enumerate(nodes[:k]):
             if np.random.rand() < edge_prob:
                 edge = Edge(node0, "adjacent_to", node1)
                 graph.add_edge(edge)
@@ -89,18 +83,84 @@ def _create_random_graph(r, graphname="random", n_nodes=1000, edge_prob=0.1):
     edges = _add_edges(nodes, redis_graph, edge_prob)
     return redis_graph
 
+
+def _query_redis(r, graphname, query):
+    res = r.execute_command("GRAPH.QUERY", graphname, query)
+    return QueryResult(res[0], res[1])
+
+def _get_nodes(r, graphname):
+    query = "MATCH (n) RETURN (n)"
+    ret = _query_redis(r, graphname, query)
+    node_list = [x[0] for x in ret.result_set[1:]]
+    return node_list
+
+def _get_edges(r, graphname):
+    query = "MATCH (n)-[r]->(m) RETURN n,r,m"
+    ret = _query_redis(r, graphname, query)
+    return ret.result_set[1:]
+
+def _get_graph(r, graphname="random"):
+    g = nx.Graph()
+    nodes = _get_nodes(r, graphname)
+    edges = _get_edges(r, graphname)
+    for node in nodes:
+        g.add_node(node)
+    for edge in edges:
+        g.add_edge(edge[0],edge[2])
+    return g, _get_index_map(g)
+
+def _get_index_map(g):
+    nodelist = list(g.nodes)
+    return {x:k for k, x in enumerate(nodelist)}
+
+def _get_adjacency(g):
+    return nx.adjacency_matrix(g)
+
+def _sample_adj(adj, idxs):
+    adj_p = adj[idxs,:]
+    return adj_p[:, idxs]
+
+def _get_features(r, indexes):
+    matlist = []
+    for index in indexes:
+        matlist.append(_bytes2array(r.get(index)))
+    return np.stack(matlist, axis=-1)
+
+def _sample_subgraph(r, adj, index_map, indexes):
+    # Turn those string index keys into integer position indexes
+    idxs = [index_map[x] for x in indexes]
+    sampled_adj = _sample_adj(adj, idxs)
+
+    # Now we need to get the feature matrix for this subgraph.
+    feat_mat = _get_features(r, indexes)
+    return sampled_adj, feat_mat
+
 def _main():
     r = redis.Redis(host='localhost', port=6379)
     g = _create_random_graph(r)
     t = time.time()
     g.commit()
     print("after calling graph.commit: {}".format(time.time() - t))
-    # pprint(g.nodes.keys()[:10])
-    # print(len(g.nodes.keys()))
-    # g.delete()
-    # r.flushall()
+
+    # Now load the saved graph and features saved in redis into a nx.Graph
+    G, idx_map = _get_graph(r)
+    adj = _get_adjacency(G)
+
+    # Select a subgraph.
+    n_subgraph = 200
+    all_nodes = list(G.nodes)
+    subgraph = np.random.choice(all_nodes, size=n_subgraph, replace=False).tolist()
+
+    # These are what we need to hand over to for mini-batch GCN
+    adj, features = _sample_subgraph(r, adj, idx_map, subgraph)
+
+    # Show the adjacency matrix and print shape of features
+    plt.matshow(adj.todense())
+    plt.show()
+    print(features.shape)
+
+    # Flush redis so we can do it over again without trouble.
+    r.flushall()
 
 if __name__ == "__main__":
-    a = time.time()
     _main()
-    print(time.time() - a)
